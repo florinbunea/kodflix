d06908e59398f864c79332d30ef2eb1cdbc36749
florinbunea <bunea.florin18@gmail.com>
2019-04-01 16:40:27 +0100

    add Venom image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import venomLogo from './Venom.jpeg';
+import './App.css';
+
+
 import './App.css';
 
 class App extends Component {
@@ -7,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={venomLogo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
