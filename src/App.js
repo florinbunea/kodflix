71227fd98f93fe014a5230a4c42c095d4e3bc814
florinbunea <bunea.florin18@gmail.com>
2019-04-03 12:32:29 +0100

    title overlay #5

--- src/App.js
+++ src/App.js
@@ -16,28 +16,46 @@ class App extends Component {
         <div className='container'>
           <div className='item'>
             <img src={BlackMirror} alt='Black Mirror.jpg' />
+            <div className='overlay'>
+              <h1>BlackMirror</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={GameofThornes} alt='Game Of Thrones.jpg' />
+            <div className='overlay'>
+              <h1>GameofThrones</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={MeninBlack} alt='Men in Black.jpg' />
+            <div className='overlay'>
+              <h1>Man in Black</h1>
+            </div>
           </div>
         </div>
         <div className='container'>
           <div className='item'>
             <img src={starwars} alt='Star Wars.jpg' />
+            <div className='overlay'>
+              <h1>Star Wars</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={TheWalkingDead} alt='The Walking Dead.jpg' />
+            <div className='overlay'>
+              <h1>The Walking Dead</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={Venom} alt='Venom.jpg' />
+            <div className='overlay'>
+              <h1>Venom</h1>
+            </div>
           </div>
         </div>
       </div>
-     );
-    }
+    );
   }
-                                      
+}
+
 export default App;
