409889f315dc608ffc811f9eb62bbb6144d61899
florinbunea <bunea.florin18@gmail.com>
2019-04-05 10:59:14 +0100

    #8 introduce routing/navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Gallery from './Gallery';
+import Details from './Details';
 
 
 import './App.css';
@@ -7,12 +9,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <Router>>
       <div className="App">
-        <Gallery />
-      </div>
+          <Route exact path='/' component={Gallery} />
+          <Route exact path='/details' component={Details} />
+        </div>
+      </Router>
     );
   }
 }
+
+
 
 
 export default App;
