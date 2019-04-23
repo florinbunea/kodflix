8d27cf35c11dde0e11c99376f6fa6674a8d08a64
florinbunea <bunea.florin18@gmail.com>
2019-04-23 12:27:08 +0100

    #correction of some mistakes

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
         <div>
-            <h1>Ooops, it looks like this page doesn't exist :(</h1>
+            <h1>Ooops, it looks like this page doesn't exist :(</h1>;
             <Link to='/'><h1 className='return'>Home Page</h1></Link>
         </div>
     );
