16225ea2c05897939338a118854232e5172278fb
florinbunea <bunea.florin18@gmail.com>
2019-04-26 11:53:38 +0100

    #19 return the list of shows form backend

---
+++ src/backend/Gallery-get.js
@@ -0,0 +1,16 @@
+module.exports = function getTvShow() {
+  return [
+    { id: 'Black Mirror', name: 'Black Mirror', 
+     details: 'An anthology series exploring a twisted, high-tech world where humanity is greatest innovations and darkest instincts collide.' },
+    { id: 'Game of Thrones', name: 'Game of Thrones', 
+    details: 'Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years.' },
+    { id: 'Men in Black', name: 'Men in Black',  
+    details: 'A police officer joins a secret organization that polices and monitors extraterrestrial interactions on Earth.' },
+    { id: 'Star Wars', name: 'Star Wars',  
+    details: 'Three decades after the Empire is defeat, a new threat arises in the militant First Order. Defected stormtrooper Finn and the scavenger Rey are caught up in the Resistance is search for the missing Luke Skywalker.'},
+    { id: 'The Walking Dead', name: 'The Walking Dead',  
+    details: 'Sheriff Deputy Rick Grimes wakes up from a coma to learn the world is in ruins, and must lead a group of survivors to stay alive.'},
+    { id: 'Venom', name: 'Venom', 
+    details: 'A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. But the being takes a liking to Earth and decides to protect it.' },
+  ];
+}
--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const getTvShow = require('./Gallery-get.js');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/Gallery-get', (req, res) => res.send(getTvShow()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
