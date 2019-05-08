6c41ee0257ecad6596660a93b8cb4bc4de9adef1
Florin Bunea <danflorinbunea@Florin-Buneas-MacBook-Pro.local>
2019-05-08 14:29:16 +0100

    # improve setuo of backend & updates

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
 const getTvShow = require('./Gallery-get.js');
 
-app.get('/rest/Gallery-get', (req, res) => res.send(getTvShow()))
+app.get('/rest/Gallery-get', (req, res) => res.send(getTvShow()));
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
