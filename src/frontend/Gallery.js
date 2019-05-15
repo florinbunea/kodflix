8a234b2236073b40e22d9ed433b04dd2c525a06d
Florin Bunea <danflorinbunea@Alecs-MBP-3.localdomain>
2019-05-15 12:18:40 +0100

    # 22 connect frondend / backend servers

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -3,18 +3,27 @@ import TvShow from './TvShow';
 import getTvShow from './Gallery-get';
 
 export default function Gallery() {
+
+  fetch('/rest/Gallery-get')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(JSON.stringify(myJson));
+    });
+
   return (
     <div>
       <div className='container'>
         {
           getTvShow().map(tvShow => (
-           <TvShow 
-              key={tvShow.id}  
-              id={tvShow.id} 
-              name={tvShow.name} 
+            <TvShow
+              key={tvShow.id}
+              id={tvShow.id}
+              name={tvShow.name}
               logo={tvShow.logo} />
           ))
-        } 
+        }
       </div>
     </div>
   );
