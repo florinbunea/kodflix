1e8c3c65219a56def756bc6ad9bd894b634448ac
Florin Bunea <danflorinbunea@Alecs-MBP-3.localdomain>
2019-05-15 16:04:34 +0100

    #23 enable the backend server to prod

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3001;
+const port = process.env.Port || 3001;
 const getTvShow = require('./Gallery-get.js');
+const pathToBeFolder = '../../build';
+
+app.use(express.static(path.join(__dirname, pathToBeFolder)));
 
 app.get('/rest/Gallery-get', (req, res) => res.send(getTvShow()));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, pathToBeFolder, 'index.html'));
+});
