58d39e747c2a0c3193a5e3c3eb88dbd0f0f74bff
Florin Bunea <danflorinbunea@Alecs-MBP-3.localdomain>
2019-05-15 16:25:59 +0100

    #23 updates

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.Port || 3001;
+const port = process.env.PORT || 3001;
 const getTvShow = require('./Gallery-get.js');
 const pathToBeFolder = '../../build';
 
